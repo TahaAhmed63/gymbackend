1178f224fcba416217965ba40f12127b3821da31
TahaAhmed63 <tahakap234@gmail.com>
2025-05-25 02:30:59 +0500

    done

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -11,7 +11,7 @@ const getAllPayments = async (req, res, next) => {
     const { member_id, start_date, end_date } = req.query;
     const pagination = getPaginationParams(req);
     const gym_id = req.user.gym_id;
-    
+    console.log(gym_id,"gym_id")
     // Build query
     let query = supabaseClient
       .from('payments')
